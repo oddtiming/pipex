d3ad825ff890e1061a240509a1045204b7f6e373
Ismael Yahyaoui Racine <ismaelyahyaouiracine@Ismaels-MacBook-Pro.local>
2022-01-24 12:52:20 -0500

    macbook -> school

--- incl/pipex.h
+++ incl/pipex.h
@@ -5,6 +5,7 @@
 # include <unistd.h>
 # include <errno.h>
 # include <fcntl.h>
+# include "../libft/libft.h"
 
 //TO REMOVE
 # include <stdio.h>
@@ -26,10 +27,20 @@ typedef struct s_args
 char	*get_env_var(char const *envp[], char *var_name);
 void	ft_print_split(char **split_arr, char *name);
 void	ft_strcat_iter(char **vector, char *to_cat);
+char	*ft_get_last_token(char *string, char delimiter);
+
+//CLEANUP FUNCTIONS
+void	pipex_cleanup_args(t_args *args);
 
 //INIT FUNCTIONS
 void	args_init(t_args **args);
-void	parse_args(t_args *args, int argc, char const **argv, char const **envp);
+void	pipex_parse_args(t_args *args, int argc, char const **argv, char const **envp);
+
+//REDIRECTIONS
+void	pipex_redirect_in_file(t_args *args);
+
+//EXECUTION
+void	pipex_execute_cmds(t_args *args);
 
 //TEST FUNCTIONS
 void	access_test(t_args *args);
--- src/ft_pipex_execute.c
+++ src/ft_pipex_execute.c
@@ -4,6 +4,7 @@ static int	execute_cmd_n(t_args *args, int cmd_index)
 {
 	int		i;
 	char	*curr_path;
+	int		pipe_fds[2];
 
 	i = 0;
 	while (args->pathv[i])
@@ -16,16 +17,31 @@ static int	execute_cmd_n(t_args *args, int cmd_index)
 	return (-1);
 }
 
-void	pipex_execute_cmds(t_args *args)
+void	pipex_execute_cmds(t_args *args, int write_end)
 {
 	static int	cmd_index;
 	int			pipe_fds[2];
-	pid_t		child_pid;
+	int			status;
+	pid_t		pid_child;
 
 	if (cmd_index < (args->cmds_count - 1));
+	{
 		pipe(pipe_fds);
-	child_pid = fork();
+		//write the file-redirected stream
+	}
+	pid_child = fork();
+	dup2(write_end, pipe_fds[1]);
 	if (child_pid == 0)
+		//following function needs the pipe fds
+		//Needs to redirect stdout onto a write end of a pipe
 		execute_cmd_n(args, cmd_index);
+	close(pipe_fds[1]);
+	close(pipe_fds[0]);
+	waitpid(pid_child, &status, E)
+	if (cmd_index == args->cmds_count - 1)
+	{
+		return ;
+	}
+	cmd_index+=1;
 
 }
